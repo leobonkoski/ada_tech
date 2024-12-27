eefae21db152731eeaaefcbe3f022f007d8858e1
Leonardo Bonksoki <leonardobonkoski@gmail.com>
2024-12-27 18:00:03 -0300

    Adicionando conteudo das primeiras aulas do curso Lógica de programação com Javascript (Node.JS) junto dos pacotes do Node eslint e prettier com suas configurações para ajudar no desenvolvimento

---
+++ node_js/vetores.js
@@ -0,0 +1,10 @@
+// Se tratando de vetores/arrays faz sentido trabalharmos apenas com elementos que façam sentido entre si ou que sejam de um mesmo tipo
+const nomedeAlunos = ["Carlos", "Fernando", "Rafaela", "Antonio"];
+const numerosPrimos = [1, 2, 3, 5, 7];
+
+console.log(nomedeAlunos);
+console.log(numerosPrimos);
+
+
+
+
